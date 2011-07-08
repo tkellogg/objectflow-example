c89b6cd199e298ef753d3b8b4fda51f5693e2017
Tim Kellogg <tkellogg@kpaonline.com>
2011-07-08 14:43:41 -0600

    Added basic MVC project

---
+++ objectflow-example/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace objectflow_example.Models
+{
+	public class Position
+	{
+		public virtual int PositionId { get; set; }
+	}
+}
